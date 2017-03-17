085966e305ad630e75208c2d3f4c71216c60b5be
Taka <yyyymmddhhmmsssss@gmail.com>
2017-03-17 18:28:14 +0900

    画像の入れ替え Utilの拡充、衝突の不具合修正

--- SpaceInvadian/GameUtil.cs
+++ SpaceInvadian/GameUtil.cs
@@ -14,13 +14,27 @@ namespace SpaceInvadian
         /// </summary>
         public static bool IsCollision(FrameworkElement a, FrameworkElement b)
         {
-            double x_abs = Math.Abs((Canvas.GetTop(a) + a.Width / 2) - (Canvas.GetTop(b) + a.Width / 2));
-            double y_abs = Math.Abs((Canvas.GetLeft(a) + a.Height / 2) - (Canvas.GetLeft(b) + a.Height / 2));
+            double x_abs = Math.Abs((Canvas.GetTop(a) + a.Width / 2) - (Canvas.GetTop(b) + b.Width / 2));
+            double y_abs = Math.Abs((Canvas.GetLeft(a) + a.Height / 2) - (Canvas.GetLeft(b) + b.Height / 2));
 
             double aw = a.Width / 2 + b.Width / 2;
             double ah = a.Height / 2 + b.Height / 2;
 
             return x_abs < aw && y_abs < ah;
+        }
+
+        /// <summary>
+        /// 指定した画像の中央座標を取得します。
+        /// Tuple
+        ///   - Item1 : X-Center
+        ///   - Item2 : Y-Center
+        /// </summary>
+        public static Tuple<double, double> GetCenter(FrameworkElement src)
+        {
+            double left = Canvas.GetLeft(src) + src.Width / 2;
+            double top = Canvas.GetTop(src) + src.Height / 2;
+
+            return new Tuple<double, double>(left, top);
         }
     }
 }
